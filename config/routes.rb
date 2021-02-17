8ccb98d6a6a52444aec0aad8b338ec1ad9a1a6a4
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-02-17 09:44:41 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  
-  get "/" => "posts#home"
-  get "posts/new" => "posts#new"
-
-  post "posts/index" => "posts#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
